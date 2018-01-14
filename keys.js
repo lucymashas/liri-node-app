6f26eddaf7b5981bc46c2ef3adf919dd58cb2eae
Luz Mashas <luzmashas@Luzs-MacBook-Pro.local>
2018-01-14 11:40:15 -0500

    update to init files

--- keys.js
+++ keys.js
@@ -2,10 +2,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '<kNfIBFY99aW0eqeyyqtxWghow>',
+  consumer_secret: '<KKaGpPX6LPwRPlKJj2HJK6ZwUTWk2TosMqwOJEaft7z7eqMyyh>',
+  access_token_key: '<952573916610023431-jzaWmEOYFSMjlYzFfezyjgOwdOxC6ou>',
+  access_token_secret: '<YQkQZQOwdgYKXR3HvynSRhK1DfYyvjVCzpqFCh4TbLz8m>',
 }
 
 module.exports = twitterKeys;
